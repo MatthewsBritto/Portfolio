0fb9adfa0087951df63dda1dae100058707552f2
unknown <luismatthews29@gmail.com>
2022-08-31 08:54:55 -0300

    add animaçoes nav

--- Js/main.js
+++ Js/main.js
@@ -3,9 +3,9 @@ var themeColor = document.querySelector('.day-night');
 var selector = document.getElementById('themeSelector');
 var body = document.querySelector('body');
 
-themeColor.addEventListener( "click", changeTheme);
+themeColor.addEventListener("click", changeTheme);
 
-function toLight(){
+function toLight() {
 
     body.classList.remove('dark');
     selector.classList.replace('fa-sun', 'fa-moon');
@@ -13,7 +13,7 @@ function toLight(){
 
 }
 
-function toDark(){
+function toDark() {
 
     body.classList.add('dark');
     selector.classList.replace('fa-moon', 'fa-sun');
@@ -22,7 +22,76 @@ function toDark(){
 
 function changeTheme() {
     // Verify and change the theme!
-    body.classList.contains('dark') ? 
-    toLight() :       
-    toDark();
+    body.classList.contains('dark') ?
+        toLight() :
+        toDark();
+}
+
+
+// Nav/ Pages animations
+
+var
+    nav = document.querySelectorAll('li');
+    allNav = nav.length;
+    sec = document.querySelectorAll('.section');
+    
+
+for (i = 0; i < allNav; i++) {
+
+    // isola cada botão dentro de uma estrutura de repetição
+    const a = nav[i].querySelector("a");
+
+
+    a.addEventListener('click', function () {
+
+        
+
+        for (i = 0; i < sec.length; i++) {
+
+            // tira todos os back-section
+            sec[i].classList.remove('back-section');
+            
+
+        }
+        for( j = 0 ; j < allNav ; j++){
+            // Retira as classe de animação dos itens  
+            if(nav[j].querySelector('i').classList.contains('flutter')){
+                nav[j].querySelector('i').classList.remove('flutter')
+            }         
+        }
+
+        for ( n = 0; n < allNav; n++) {
+            if (nav[n].querySelector('a').classList.contains('active')) {
+                // add back-section ao elemento atual antes da troca
+                sec[n].classList.add('back-section');
+
+            }
+            // retira a classe ativo do elemento anterior
+            nav[n].querySelector('a').classList.remove('active');
+            
+        }
+        // adiciona ativo ao novo elemento
+        this.classList.add('active');
+        
+        
+        showSection(this,this.children[0])
+    })
+
+
+}
+
+
+function showSection(element,icon) {
+
+    for (i = 0; i < sec.length; i++) {
+
+        sec[i].classList.remove('active');
+        
+    }
+    const target = element.getAttribute("href").split("#")[1];
+    document.querySelector("#" + target).classList.add("active");
+
+    // adiciona animação ao novo elemento
+    icon.classList.add('flutter')
+
 }
